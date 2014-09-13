829cb38d403a4a59a08ed42fc247fb842e29971f
Marius Utheim <marius_utheim@hotmail.com>
2014-09-13 20:00:25 +0200

    Fixed sound bug

--- HappyCloud.cs
+++ HappyCloud.cs
@@ -6,10 +6,10 @@ namespace Project1
     public class HappyCloud : SceneryObject
     {
         public static readonly Sound HappyCloudSound = new Sound(Properties.Resources.HappyCloudSoundName);
-        public const int ScoreAward = 4;
-        public const int EngeryAward = 32;
         public static readonly Sprite HappyCloudSprite1 = new Sprite(Properties.Resources.HappyCloudFileName1, origin: IntVector.Zero);
         public static readonly Sprite HappyCloudSprite2 = new Sprite(Properties.Resources.HappyCloudFileName2, origin: IntVector.Zero);
+        public const int ScoreAward = 4;
+        public const int EngeryAward = 32;
 
         public HappyCloud()
             : base(Room.Width + 50, GRandom.Int(100, 300))
