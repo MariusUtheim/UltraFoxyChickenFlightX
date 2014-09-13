677c05a3016953c7b5c4a7fc30e8d9a029619e2a
Håkon Wallann <hwallann@gmail.com>
2014-09-13 16:06:42 +0200

    HappyCloud

---
+++ HappyCloud.cs
@@ -0,0 +1,11 @@
+﻿using GameMaker;
+using System;
+
+namespace Project1
+{
+    public class HappyCloud : SceneryObject
+    {
+        public HappyCloud(double x, double y) : base(x, y) { }
+        public HappyCloud(Point location) : base(location) { }
+    }
+}
