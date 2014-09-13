3504c84594bc75eb1808c3148cca4ac72915d6af
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2014-09-13 17:36:12 +0200

    Enegery/Score output to window title

--- Program.cs
+++ Program.cs
@@ -7,10 +7,20 @@ namespace Project1
     {
         static int Main(string[] args)
         {
+            Statistics.EnergyChanged += OnStatisticsChanged;
+            Statistics.ScoreChanged += OnStatisticsChanged;
+
             var gameroom = new Room(1024, 768);
             Game.Run(gameroom, gameStart: GameStartup);
 
             return 0;
+        }
+
+        private static void OnStatisticsChanged(int ignoreValue)
+        {
+            Window.Title = string.Format("{0} - Energy: {1:N0} - Score: {2:N0}",
+                "ULTRA FOXY CHICKEN FLIGHT X",
+                Statistics.Energy, Statistics.Score);
         }
 
         static void GameStartup()
