90af89e85c6289087b3538fdb9cc114d61c3e195
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2014-09-13 21:43:45 +0200

    Sanitized location values for farmer, good/bad tree

--- GoodTree.cs
+++ GoodTree.cs
@@ -8,10 +8,10 @@ namespace Project1
         public static readonly Vector BoostAward = new Vector(0, -10);
 
 		public GoodTree() 
-			: base(Room.Width + 50, 520)
+			: base(Room.Width + 50, 450)
 		{
 			this.Sprite = Sprites.GoodTree;
-			this.Transform.Scale *= 0.2;
+			this.Transform.Scale *= 0.25;
 			this.Mask.Rectangle(55, 66, 726 - 55, this.Sprite.Height - 66);
 		}
 
