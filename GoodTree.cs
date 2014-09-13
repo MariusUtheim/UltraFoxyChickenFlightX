77a6bb4bd7f830e0e477b3905fa395788b0bcb52
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2014-09-13 19:12:51 +0200

    Increased boost for good tree collision

--- GoodTree.cs
+++ GoodTree.cs
@@ -5,7 +5,7 @@ namespace Project1
 {
     public class GoodTree : SceneryObject
     {
-        public static readonly Vector BoostAward = new Vector(0, -8);
+        public static readonly Vector BoostAward = new Vector(0, -10);
         public static readonly Sprite GoodTreeSprite = new Sprite(Properties.Resources.GoodTreeSpriteFileName, origin: IntVector.Zero);
 
 		public GoodTree() 
