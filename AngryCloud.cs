9fd92d822bf335e34a732c7454f2831921595ebf
Marius Utheim <marius_utheim@hotmail.com>
2014-09-13 17:18:48 +0200

    No comment

--- AngryCloud.cs
+++ AngryCloud.cs
@@ -5,6 +5,7 @@ namespace Project1
 {
     public class AngryCloud : SceneryObject
     {
+		public const int ScorePenalty = 5;
 		private static Sprite AngryCloudSprite = new Sprite(Properties.Resources.AngryCloudSpriteFileName, origin: IntVector.Zero);
 
 		public AngryCloud() : base(Room.Width + 50, GRandom.Int(100, 300))
